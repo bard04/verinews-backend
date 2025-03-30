fe8af922336e654b4cfb18faad10e478a0764ca7
Evans Mageto <evansmageto55@gmail.com>
2025-03-30 17:59:30 +0300

    Added /currents route

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
     res.send('VeriNews Backend is running!');
 });
 
+// Added /currents route
+app.get('/currents', (req, res) => {
+    res.json({ message: "Currents news data should appear here" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
